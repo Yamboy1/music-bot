ea3c53e15697d44885430f0d2e4a1a2b966b89e8
Yamboy1 <yamboyd1@gmail.com>
2019-07-27 22:05:36 +1200

    Extra intial setup

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const { CommandoClient } = require("discord.js-commando");
 const path = require("path");
 
+const Keyv = require('keyv');
+const KeyvProvider = require('commando-provider-keyv');
+
 const { token } = require("./config.js");
 
 const client = new CommandoClient({
 	commandPrefix: "m!",
 	owner: "404813853851910155"
 });
+
+client.setProvider(new KeyvProvider(new Keyv()));
 
 client.registry
 	.registerDefaultTypes()
@@ -18,8 +23,15 @@ client.registry
 	.registerDefaultCommands()
 	.registerCommandsIn(path.join(__dirname, "commands"));
 
-client.once("ready", () => console.log(`Logged in as ${client.user.tag}`);
+client.once("ready", () => console.log(`Logged in as ${client.user.tag}`));
 
 client.on("error", console.error);
+
+
+["SIGINT", "SIGUSR1", "SIGUSR2", "SIGTERM"].forEach(event =>
+	process.on(event, (async event => {
+		await client.destroy();
+		console.log(`And... that's all folks! (${event})`)
+	}).bind(null,event)));
 
 client.login(token);
